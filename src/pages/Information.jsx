4350299a56b74ba456167ab85894756f6aab5944
Daniel C <50636323+floranova13@users.noreply.github.com>
2022-04-24 02:03:59 -0400

    Attach to backend as a test

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fetch from 'node-fetch';
+
+const fetchBlightsources = async () => {
+  const response = await fetch(
+    'https://blightsources-backend.herokuapp.com/api/blightsources'
+  );
+  const data = await response.json();
+
+  return data;
+};
 
 const Information = () => {
+  const [blightsources, setBlightsources] = useState([]);
+
+  useEffect(() => {
+    setBlightsources(fetchBlightsources());
+  }, []);
+
   return (
-    <div className='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
-      <div className='-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap'>
-        <div className='ml-4 mt-2'>
-          <h3 className='text-lg leading-6 font-medium text-gray-900'>
-            Job Postings
-          </h3>
-        </div>
-        <div className='ml-4 mt-2 flex-shrink-0'>
-          <button
-            type='button'
-            className='relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-          >
-            Create new job
-          </button>
-        </div>
-      </div>
-    </div>
+    <ul>
+      {blightsources.map((b, i) => {
+        return <li key={b.name}>{b.name}</li>;
+      })}
+    </ul>
   );
 };
 
